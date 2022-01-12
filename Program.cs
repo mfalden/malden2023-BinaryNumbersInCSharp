5746e555f48598c9daddf5a750c1d3ade9117563
mfalden <malden2023@chadwickschool.org>
2022-01-12 12:12:26 -0800

    chore: Initialize project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace malden2023_BinaryNumbersInCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
